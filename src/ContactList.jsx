57ea0329d50e88970b080aa8f501c3be4822ad60
victor <vvazquez307@gmail.com>
2023-02-15 12:56:02 -0600

    contactlist

--- src/ContactList.jsx
+++ src/ContactList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const ContactList = (props) => {
+  console.log(props)
   return (
   <table>
       <tbody>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -16,7 +16,7 @@ const Main = () => {
         <div>Contact List</div>
       </div>
       <div id="container">
-        <ContactList contacts="{contacts}"> </ContactList>
+        <ContactList contacts={contacts}> </ContactList>
       </div>
     </div>
   );
